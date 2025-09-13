09822a4767a464a599ae2d18cf8fb0166255cb94
TypeNora <125792315+TypeNora@users.noreply.github.com>
2025-09-14 00:14:46 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'roulette-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const url = new URL(e.request.url);
+      if (url.origin === location.origin) {
+        const respClone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, respClone));
+      }
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
